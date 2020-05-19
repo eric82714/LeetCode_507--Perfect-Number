93b1b854abb57d4565bbdab832afc911618a84c8
eric82714 <0958820714ab@gmail.com>
2020-05-19 08:21:06 +0800

    Create perfect_number.cpp

---
+++ C++/perfect_number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool checkPerfectNumber(int num) {
+        if(num == 1) return false;
+        
+        int tmp = num-1, divisor = 2;
+        
+        while(divisor * divisor < num) {
+            if(num % divisor == 0)
+                tmp = tmp - divisor - (num / divisor);             
+            divisor ++;
+        }
+            
+        return tmp == 0;
+    }
+};
